ce736332a53e734b000d988ebb3c88b877686918
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-20 11:26:19 +0530

    commit final hero form changed

--- src/components/Home/Model.jsx
+++ src/components/Home/Model.jsx
@@ -8,7 +8,7 @@ const Model = ({ isModalOpen, setIsModalOpen }) => {
     companyName: "",
     websiteUrl: "",
     email: "",
-    fromWhere: "Digital marketing services in mumbai",
+    fromWhere: "Digital marketing services in dallas",
   });
 
   const handleSubmit = async (e) => {
@@ -169,6 +169,93 @@ const Model = ({ isModalOpen, setIsModalOpen }) => {
                 Phone No
               </label>
             </div>
+            {/* <div className="w-full relative group mt-[25px]">
+              <input
+                type="text"
+                name="name"
+                id={"name3"}
+                required
+                value={formData.name}
+                onChange={handleChange}
+                className="w-full  h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500 border-b border-[#999]"
+              />
+              <label
+                htmlFor={"name3"}
+                className="transform transition-all text-[#999] absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[15px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
+              >
+                Name
+              </label>
+            </div>
+
+            <div className="w-full relative group mt-[25px]">
+              <input
+                type="text"
+                name="companyName"
+                id={"companyname3"}
+                value={formData.companyName}
+                onChange={handleChange}
+                required
+                className="w-full  h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500 border-b border-[#999]"
+              />
+              <label
+                htmlFor={"companyname3"}
+                className="transform transition-all text-[#999] absolute top-0  left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[15px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
+              >
+                Company Name
+              </label>
+            </div>
+            <div className="w-full relative group mt-[25px]">
+              <input
+                type="text"
+                name="websiteUrl"
+                id={"webUrl"}
+                value={formData.websiteUrl}
+                onChange={handleChange}
+                required
+                className="w-full  h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500 border-b border-[#999]"
+              />
+              <label
+                htmlFor={"webUrl"}
+                className="transform transition-all text-[#999] absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[15px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
+              >
+                Website Url
+              </label>
+            </div>
+            <div className="w-full relative group mt-[25px]">
+              <input
+                type="email"
+                name="email"
+                id={"email3"}
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="w-full  h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500 border-b border-[#999]"
+              />
+              <label
+                htmlFor={"email3"}
+                className="transform transition-all text-[#999] absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[15px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
+              >
+                Email
+              </label>
+            </div>
+
+            <div className="w-full relative group mt-[25px]">
+              <input
+                type="tel"
+                name="phoneNo"
+                id={"phoneNo3"}
+                value={formData.phoneNo}
+                onChange={handleChange}
+                required
+                className="w-full  h-3 py-4 px-4 text-sm peer bg-transparent outline-none focus:border-blue-500 border-b border-[#999]"
+              />
+              <label
+                htmlFor={"phoneNo3"}
+                className="transform transition-all text-[#999] absolute top-0 left-0 h-full flex items-center pl-1 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-[15px] peer-valid:-translate-y-full group-focus-within:pl-1 peer-valid:pl-1"
+              >
+                Phone No
+              </label>
+            </div> */}
 
             <button className="py-3 rounded-xl bg-[#e31a20] mt-8 w-full font-poppins text-white text-[14px] md:text-[14px] font-bold">
               SEND REQUEST
