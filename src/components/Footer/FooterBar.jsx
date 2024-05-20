16cb043c832766635b1177a056a7a5a8303bd4cb
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-20 11:35:34 +0530

    commit dallas added everywhere

--- src/components/Footer/FooterBar.jsx
+++ src/components/Footer/FooterBar.jsx
@@ -20,7 +20,7 @@ const FooterBar = () => {
         </div>
         <div>
           <h4 class=" text-white text-[18px] text leading-tight md:text-[22px] font-redhat  font-bold">
-            Best Digital Marketing Company in India
+            Best Digital Marketing Company in Dallas
           </h4>
         </div>
         <div>
